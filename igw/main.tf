01ba1106a0b518f10c658ba6cb48044f2cf87981
Tirupathi1729 <kakarlatirupathireddy@gmail.com>
2023-08-17 21:28:03 +0530

    terraform

--- igw/main.tf
+++ igw/main.tf
@@ -5,7 +5,7 @@ resource "aws_internet_gateway" "gw" {
     Name = "gateway"
   }
 }
-resource "aws_internet_gateway_attachment" "example" {
-  internet_gateway_id = aws_internet_gateway.gw
-  vpc_id              = var.vpc_id
-}
+#resource "aws_internet_gateway_attachment" "example" {
+#  internet_gateway_id = aws_internet_gateway.gw
+#  vpc_id              = var.vpc_id
+#}
