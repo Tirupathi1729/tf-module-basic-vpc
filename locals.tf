1af4bcfa45939f377b1724a8ee455bc2010cc84c
Tirupathi1729 <kakarlatirupathireddy@gmail.com>
2023-08-18 10:03:33 +0530

    terraform

---
+++ locals.tf
@@ -0,0 +1,12 @@
+locals {
+  public_subnet_ids = [for k,v in lookup(lookup(module.subnets,"public",null),"subnet_ids",null): v.id]
+  app_subnet_ids = [for k,v in lookup(lookup(module.subnets,"app",null),"subnet_ids",null): v.id]
+  db_subnet_ids = [for k,v in lookup(lookup(module.subnets,"db",null),"subnet_ids",null): v.id]
+  private_subnet_ids = concat(local.app_subnet_ids,local.db_subnet_ids)
+
+  #RT
+  public_route_table_ids = [for k,v in lookup(lookup(module.subnets,"public",null),"route_table",null): v.id]
+  app_route_table_ids = [for k,v in lookup(lookup(module.subnets,"public",null),"route_table",null): v.id]
+  db_route_table_ids = [for k,v in lookup(lookup(module.subnets,"public",null),"route_table",null): v.id]
+
+}
